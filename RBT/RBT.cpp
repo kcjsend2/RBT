bf3fbc2a9badc16341460a26ea7983228bb26454
kcjsend2 <kcjsend2@naver.com>
2020-11-18 12:17:25 +0900

    구조 뜯어 고침

--- RBT/RBT.cpp
+++ RBT/RBT.cpp
@@ -1,39 +1,104 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define BLACK 0
+#define RED 1
 #include <iostream>
+
+using namespace std;
 
 class Node
 {
 public:
 	int key;
 
+	bool Color; // BLACK : 0, RED : 1
+
 	Node* Left;
 	Node* Right;
 };
 
 
-Node* Insert_BT(Node* root, int key)
+Node* Check_Tree(Node* root, bool PrevColor, int level)
 {
-	if (root == NULL)
+	if (level == 0)
 	{
-		root = (Node*)malloc(sizeof(Node));
-		root->Left = root->Right = NULL;
-
-		root->key = key;
-
-		return root;
+		if (root->Color == BLACK)
+		{
+			if(root->Left != NULL)
+				Check_Tree(root->Left, BLACK, level + 1);
+			if (root->Right != NULL)
+				Check_Tree(root->Right, BLACK, level + 1);
+		}
 	}
 	else
 	{
-		if (key < root->key)
+		if (root->Color == BLACK)
 		{
-			root->Left = Insert_BT(root->Left, key);
+			if (PrevColor == BLACK)
+			{
+				cout << "규칙 위반 발생! " << level << "층의 " << root->key << "의 값을 가진 노드" << endl;
+				cout << "사유 : 부모와 자식의 색이 같음, 색 : BLACK, NIL 아님" << endl;
+			}
+		}
+		else if (root->Color == RED)
+		{
+			if (PrevColor == RED)
+			{
+				cout << "규칙 위반 발생! " << level << "층의 " << root->key << "의 값을 가진 노드" << endl;
+				cout << "사유 : 부모와 자식의 색이 같음, 색 : RED, NIL 아님" << endl;
+			}
+		}
+	}
+}
+
+void Init_BT(Node* root, Node* NIL, int key)
+{
+	NIL = (Node*)malloc(sizeof(Node));
+	NIL->Left = NULL;
+	NIL->Right = NULL;
+	NIL->Color = BLACK;
+
+	Node* root = (Node*)malloc(sizeof(Node));
+	root->key = key;
+	root->Left = NIL;
+	root->Right = NIL;
+	root->Color = BLACK;
+}
+
+Node* Insert_BT(Node* root, Node* NIL, int key, int level)
+{
+	if (root->key < key)
+	{
+		if (root->Left == NIL)
+		{
+			Node* newNode = (Node*)malloc(sizeof(Node));
+			newNode->key = key;
+			newNode->Left = NIL;
+			newNode->Right = NIL;
+			newNode->Color = RED;
+			root->Left = NIL;
 		}
 		else
 		{
-			root->Right = Insert_BT(root->Right, key);
+			Insert_BT(root->Left, NIL, key, level + 1);
 		}
 	}
-	return root;
+	else if (root->key > key)
+	{
+		if (root->Right == NIL)
+		{
+			Node* newNode = (Node*)malloc(sizeof(Node));
+			newNode->key = key;
+			newNode->Left = NIL;
+			newNode->Right = NIL;
+			newNode->Color = RED;
+			root->Right = NIL;
+		}
+		else
+		{
+			Insert_BT(root->Right, NIL, key, level + 1);
+		}
+	}
+
 }
 
 void Delete_BT(Node* root, int key)
@@ -103,10 +168,10 @@ void Delete_BT(Node* root, int key)
 
 int main()
 {
+	Node* NIL = NULL;
 	Node* root = NULL;
-	root = Insert_BT(root, 3);
-	root = Insert_BT(root, 4);
-	root = Insert_BT(root, 2);
-	Delete_BT(root, 2);
-	std::cout << "commit test" << std::endl;
+
+	Init_BT(root, NIL, 0);
+
+
 }
